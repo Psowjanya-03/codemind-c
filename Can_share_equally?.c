ffcf2a650e42a124b9a025ae580ea5e24b492d9c
Psowjanya-03 <86092446+Psowjanya-03@users.noreply.github.com>
2023-01-20 20:43:38 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     if(a==0 && b%2==0)     {         printf("YES");     }     else if(a==0 && b%2!=0)     {         printf("NO");     }     else if((a+(2*b))%2==0)     {         printf("YES");     }     else if(a==b)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Can_share_equally?.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a==0 && b%2==0)
+    {
+        printf("YES");
+    }
+    else if(a==0 && b%2!=0)
+    {
+        printf("NO");
+    }
+    else if((a+(2*b))%2==0)
+    {
+        printf("YES");
+    }
+    else if(a==b)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
